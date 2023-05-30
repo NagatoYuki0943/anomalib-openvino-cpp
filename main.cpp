761637f950363d0042aebffafc01a284e56880d4
Yuki <workcomputer.com>
2023-05-30 14:48:09 +0800

    update some

--- main.cpp
+++ main.cpp
@@ -3,24 +3,24 @@
 
 
 int main() {
-    // 注意使用非patchcore模型时报错可以查看utils.cpp中infer_height和infer_width中的[1] 都改为 [0]，具体查看注释和metadata.json文件
+    // 注锟斤拷使锟矫凤拷patchcore模锟斤拷时锟斤拷锟斤拷锟斤拷锟皆查看utils.cpp锟斤拷infer_height锟斤拷infer_width锟叫碉拷[1] 锟斤拷锟斤拷为 [0]锟斤拷锟斤拷锟斤拷榭醋拷秃锟絤etadata.json锟侥硷拷
     string model_path = "D:/ml/code/anomalib/results/patchcore/mvtec/bottle/run/weights/openvino/model.xml";
     string meta_path  = "D:/ml/code/anomalib/results/patchcore/mvtec/bottle/run/weights/openvino/metadata.json";
     string image_path = "D:/ml/code/anomalib/datasets/MVTec/bottle/test/broken_large/000.png";
     string image_dir  = "D:/ml/code/anomalib/datasets/MVTec/bottle/test/broken_large";
-    string save_dir   = "D:/ml/code/anomalib-openvino-cpp/result"; // 注意目录不会自动创建,要手动创建才会保存
+    string save_dir   = "D:/ml/code/anomalib-openvino-cpp/result"; // 注锟斤拷目录锟斤拷锟斤拷锟皆讹拷锟斤拷锟斤拷,要锟街讹拷锟斤拷锟斤拷锟脚会保锟斤拷
     string device     = "CPU";
-    bool openvino_preprocess = true;    // 是否使用openvino图片预处理
+    bool openvino_preprocess = true;    // 锟角凤拷使锟斤拷openvino图片预锟斤拷锟斤拷
 
-    // 创建推理器
+    // 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
     auto inference = Inference(model_path, meta_path, device, openvino_preprocess);
 
-    // 单张图片推理
+    // 锟斤拷锟斤拷图片锟斤拷锟斤拷
     cv::Mat result = inference.single(image_path, save_dir);
     cv::imshow("result", result);
     cv::waitKey(0);
 
-    // 多张图片推理
-    // inference.multi(image_dir, save_dir);
+    // 锟斤拷锟斤拷图片锟斤拷锟斤拷
+    inference.multi(image_dir, save_dir);
     return 0;
 }
